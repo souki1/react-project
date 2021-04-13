656fa0d625d67bb3de6239ef1a0cd0a9f9c10ed7
souki1 <soukigirishkumar22@gmail.com>
2021-04-13 20:32:10 +0530

    react 1st project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+       <a id="inc">increment</a>
+       <p id="counter">0</p>
+     <a id="dec" onclick="dec()">decerse</a>
+    </div>
+  );
+}
+
+export default App;
